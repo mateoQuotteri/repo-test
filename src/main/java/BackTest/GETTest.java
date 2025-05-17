4780d3d82e8b0a52cbeb9b0bbc7c7d279fef022e
Mateo <quotterimateo@gmail.com>
2025-05-16 23:03:33 -0300

    segundo test con rest-assured

--- src/main/java/BackTest/GETTest.java
+++ src/main/java/BackTest/GETTest.java
@@ -1,8 +1,10 @@
 package BackTest;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
 public class GETTest {
 
@@ -15,4 +17,28 @@ public class GETTest {
         System.out.println(resGet.getHeader("Date"));
         System.out.println(resGet.getTime());
     }
+    @Test
+    public void Get_Test02() {
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+
+        int statusCode = response.statusCode();
+        JsonPath body = response.jsonPath();
+        String nombre_0 = body.getString("data.first_name[0]");
+        String mail_0 = body.getString("data.email[0]");
+        String nombre_2 = body.getString("data.first_name[2]");
+        String mail_2 = body.getString("data.email[2]");
+
+        Assert.assertEquals(statusCode, 200);
+        Assert.assertEquals(nombre_0, "Michael");
+        Assert.assertEquals(mail_0, "michael.lawson@reqres.in");
+        Assert.assertEquals(nombre_2, "Tobias");
+        Assert.assertEquals(mail_2, "tobias.funke@reqres.in");
+
+        System.out.println("El código de status es: " + statusCode);
+        System.out.println("El nombre del primer registro es: " + nombre_0);
+        System.out.println("El correo del primer registro es: " + mail_0);
+        System.out.println("El nombre del tercer registro es: " + nombre_2);
+        System.out.println("El correo del tercer registro es: " + mail_2);
+    }
+
 }
